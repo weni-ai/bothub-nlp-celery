c83d41804374604892591e90ca8bc4530225e025
Daniel Yohan <dyohan9@gmail.com>
2019-10-28 10:57:53 -0300

    [fix] Variable Environment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub-nlp-celery',
-    version='0.1.3',
+    version='0.1.4',
     description='Bothub NLP Celery',
     packages=find_packages(),
     install_requires=[
