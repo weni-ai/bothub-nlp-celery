5f793e857dccc10867a7142dbc3ac5c3f06d91c8
Daniel Yohan <dyohan9@gmail.com>
2019-10-28 10:29:33 -0300

    Shared Memory Model Spacy

--- bothub_nlp_celery/app.py
+++ bothub_nlp_celery/app.py
@@ -1,5 +1,7 @@
+import spacy
 from celery import Celery
 from kombu import Queue
+from kombu.utils.objects import cached_property
 
 from .actions import ACTION_PARSE
 from .actions import ACTION_TRAIN
@@ -8,22 +10,30 @@ from .actions import queue_name
 from . import settings
 
 
-celery_app = Celery(
-    'bothub_nlp_celery',
-    broker=settings.BOTHUB_NLP_CELERY_BROKER_URL,
-    backend=settings.BOTHUB_NLP_CELERY_BACKEND_URL)
+class CeleryService(Celery):
+    @cached_property
+    def nlp_spacy(self):
+        """Current nlp spacy instance."""
+        print(f"loading {settings.BOTHUB_NLP_LANGUAGE_QUEUE} spacy lang model...")
+        return spacy.load(settings.BOTHUB_NLP_LANGUAGE_QUEUE, parser=False)
 
-queues_name = set([
-    queue_name(ACTION_PARSE, lang)
-    for lang in settings.SUPPORTED_LANGUAGES.keys()
-] + [
-    queue_name(ACTION_TRAIN, lang)
-    for lang in settings.SUPPORTED_LANGUAGES.keys()
-] + [
-    queue_name(ACTION_EVALUATE, lang)
-    for lang in settings.SUPPORTED_LANGUAGES.keys()
-])
-celery_app.conf.task_queues = [
-    Queue(queue)
-    for queue in queues_name
-]
+
+celery_app = CeleryService(
+    "bothub_nlp_celery",
+    broker=settings.BOTHUB_NLP_CELERY_BROKER_URL,
+    backend=settings.BOTHUB_NLP_CELERY_BACKEND_URL,
+)
+
+
+nlp_language = celery_app.nlp_spacy
+
+
+queues_name = set(
+    [queue_name(ACTION_PARSE, lang) for lang in settings.SUPPORTED_LANGUAGES.keys()]
+    + [queue_name(ACTION_TRAIN, lang) for lang in settings.SUPPORTED_LANGUAGES.keys()]
+    + [
+        queue_name(ACTION_EVALUATE, lang)
+        for lang in settings.SUPPORTED_LANGUAGES.keys()
+    ]
+)
+celery_app.conf.task_queues = [Queue(queue) for queue in queues_name]
--- bothub_nlp_celery/settings.py
+++ bothub_nlp_celery/settings.py
@@ -3,35 +3,25 @@ from collections import OrderedDict
 
 
 def cast_supported_languages(i):
-    return OrderedDict([
-        x.split(':', 1) if ':' in x else (x, x) for x in
-        i.split('|')
-    ])
+    return OrderedDict([x.split(":", 1) if ":" in x else (x, x) for x in i.split("|")])
 
 
 BOTHUB_NLP_CELERY_BROKER_URL = config(
-    'BOTHUB_NLP_CELERY_BROKER_URL',
-    default='redis://localhost:6379/0'
+    "BOTHUB_NLP_CELERY_BROKER_URL", default="redis://localhost:6379/0"
 )
 
 BOTHUB_NLP_CELERY_BACKEND_URL = config(
-    'BOTHUB_NLP_CELERY_BACKEND_URL',
-    default=BOTHUB_NLP_CELERY_BROKER_URL,
+    "BOTHUB_NLP_CELERY_BACKEND_URL", default=BOTHUB_NLP_CELERY_BROKER_URL
 )
 
 BOTHUB_NLP_NLU_AGROUP_LANGUAGE_QUEUE = config(
-    'BOTHUB_NLP_NLU_AGROUP_LANGUAGE_QUEUE',
-    cast=bool,
-    default=True,
+    "BOTHUB_NLP_NLU_AGROUP_LANGUAGE_QUEUE", cast=bool, default=True
 )
 
-BOTHUB_NLP_SENTRY_CLIENT = config(
-    'BOTHUB_NLP_SENTRY_CLIENT',
-    default=None
-)
+BOTHUB_NLP_SENTRY_CLIENT = config("BOTHUB_NLP_SENTRY_CLIENT", default=None)
 
 SUPPORTED_LANGUAGES = config(
-    'SUPPORTED_LANGUAGES',
-    default='en|pt',
-    cast=cast_supported_languages,
+    "SUPPORTED_LANGUAGES", default="en|pt", cast=cast_supported_languages
 )
+
+BOTHUB_NLP_LANGUAGE_QUEUE = config("BOTHUB_NLP_LANGUAGE_QUEUE", default="en")
