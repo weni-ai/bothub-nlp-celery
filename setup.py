d13a333c101a04169a878a92dada1d2f9d5f5c8a
Daniel Yohan <dyohan9@gmail.com>
2019-11-14 11:44:28 -0300

    Added Sentry Client

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ extras_requires = {
 
 setup(
     name='bothub-nlp-celery',
-    version='0.1.5',
+    version='0.1.6',
     description='Bothub NLP Celery',
     packages=find_packages(),
     install_requires=[
         'celery==4.3.0',
         'python-decouple==3.1',
+        'sentry-sdk==0.7.10',
     ],
     extras_require=extras_requires,
 )
