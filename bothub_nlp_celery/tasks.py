0ed2fb91896c3ccc4e1aec91e3cadc244e6704ad
lucasraggi89 <lrr@ic.ufal.br>
2020-01-03 14:08:05 -0300

    add debug parse route

--- bothub_nlp_celery/tasks.py
+++ bothub_nlp_celery/tasks.py
@@ -1,3 +1,4 @@
 TASK_NLU_PARSE_TEXT = "nlu-parse-text"
+TASK_NLU_DEBUG_PARSE_TEXT = "nlu-debug-parse-text"
 TASK_NLU_TRAIN_UPDATE = "nlu-train-update"
 TASK_NLU_EVALUATE_UPDATE = "nlu-evaluate-update"
