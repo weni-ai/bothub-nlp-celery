30ae6ccbb105131f75eea009f9d5063066c37c2f
Daniel Yohan <dyohan9@gmail.com>
2020-06-30 11:22:29 -0300

    Bump version v0.1.17

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extras_requires = {
 
 setup(
     name='bothub-nlp-celery',
-    version='0.1.16',
+    version='0.1.17',
     description='Bothub NLP Celery',
     packages=find_packages(),
     install_requires=[
