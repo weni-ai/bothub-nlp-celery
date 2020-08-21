443f785632fa3f670b6a2fad22cf9fe9e5ae90f9
Lucas Ribeiro Raggi <lrr@ic.ufal.br>
2020-08-21 10:28:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extras_requires = {
 
 setup(
     name='bothub-nlp-celery',
-    version='0.1.23',
+    version='0.1.25',
     description='Bothub NLP Celery',
     packages=find_packages(),
     install_requires=[
