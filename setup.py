359c42dee1487bfcde01f8badae9e973ff51ea3c
Lucas Raggi <lrr@ic.ufal.br>
2020-10-30 10:09:25 -0300

    update version to 29staging1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extras_requires = {
 
 setup(
     name='bothub-nlp-celery',
-    version='0.1.28',
+    version='0.1.29staging1',
     description='Bothub NLP Celery',
     packages=find_packages(),
     install_requires=[
