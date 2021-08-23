6d8474f755c5e7a4280059b52b4317d391bf75fd
Lucas Agra <lucasomena@hotmail.com>
2021-08-23 17:08:29 -0300

    Changing queue name logic

--- bothub_nlp_celery/actions.py
+++ bothub_nlp_celery/actions.py
@@ -10,12 +10,34 @@ ACTION_EVALUATE = "evaluate"
 
 
 def queue_name(language, action=None, model_name=None):
-    queue = language
-    if model_name == "QA":
-        if language not in ["en", "pt_br"]:
-            queue = f"multilang-{model_name}"
+    if model_name == "SPACY":
+        if language == "pt_br" and action in [ACTION_SENTENCE_SUGGESTION, ACTION_INTENT_SENTENCE_SUGGESTION, ACTION_WORD_SUGGESTION]:
+            queue = "pt_br-SPACY_SUGGESTION"
+        elif language in ["en", "pt_br", "es", "ru", "fr", "pt"]:
+            queue = language + "-SPACY"
         else:
-            queue += f"-{model_name}"
+            queue = "multilang"
     elif model_name is not None:
-        queue += f"-{model_name}"
+        if language in ["en", "pt_br"]:
+            queue = language + "-" + model_name
+        else:
+            queue = "multilang-" + model_name
+    else:
+        if language in ["en", "pt_br"]:
+            queue = language
+        else:
+            queue = "multilang"
+
     return queue
+
+#
+# def queue_name(language, action=None, model_name=None):
+#     queue = language
+#     if model_name == "QA":
+#         if language not in ["en", "pt_br"]:
+#             queue = f"multilang-{model_name}"
+#         else:
+#             queue += f"-{model_name}"
+#     elif model_name is not None:
+#         queue += f"-{model_name}"
+#     return queue
